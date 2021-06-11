cad2c4603f298b6b64ee7e30d8e3f09bdd3721a5
Paris Holley <mail@parisholley.com>
2021-06-11 16:11:13 -0400

    musl is a battle for another day

--- build.rs
+++ build.rs
@@ -14,24 +14,11 @@ fn scan(build: &mut Build, path: &str, suffix: &str) {
     }
 }
 
-#[cfg(feature = "static")]
-fn builder() -> Build {
-    return cc::Build::new();
-}
-
-#[cfg(not(feature = "static"))]
-fn builder() -> Build {
-    let mut build = cc::Build::new();
-    build.static_flag(false);
-
-    return build;
-}
-
 fn main() {
     println!("cargo:rerun-if-changed=src/shim/wrapper.h");
     println!("cargo:rerun-if-changed=src/shim/wrapper.c");
 
-    let mut c = builder();
+    let mut c = cc::Build::new();
     c.warnings(false);
 
     scan(&mut c, "./src/shim/", ".c");
